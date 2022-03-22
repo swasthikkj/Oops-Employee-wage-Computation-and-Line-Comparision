4ff08a7c14aabb9e2a5cd1a10aab8f8f6c1530df
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-03-22 17:53:04 +0530

    program

---
+++ Line Comparision 3/src/UC3.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class UC3 {
+	    static final double lengthl1 = length1();
+	    static final double lengthl2 = length2();
+
+	    public static void main(String[] args){
+	        System.out.println("The length of the line l1 is "+lengthl1);
+	        System.out.println("The length of the line l2 is "+lengthl2);
+	        UC3.compareTo();
+	    }
+	    static void  compareTo(){                                    
+	        if (lengthl1 == lengthl2)
+	        {
+	            System.out.println("The two lines are equal");
+	        }
+	        else if(lengthl1 >= lengthl2)
+	        {
+	            System.out.println("The first line is greater than second line");
+	        }
+	        else
+	        {
+	            System.out.println("The first line is greater than second line");
+	        }
+	    }
+	    static double length1() {                                               
+	        System.out.println("Enter Details of first line ");
+	        System.out.println();
+
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter x1 point of First line");
+	        int x1 = scanner.nextInt();
+	        System.out.println("Enter y1 point of First line");
+	        int x2 = scanner.nextInt();
+	        System.out.println("Enter x2 point of First line");	        
+	        int y1 = scanner.nextInt();
+	        System.out.println("Enter y2 point of First line");
+	        int y2 = scanner.nextInt();
+	        double length1 = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+	        return length1;
+	    }
+	static double length2(){                                           
+	        System.out.println("Enter Details of second line ");
+	        System.out.println();
+
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter x1 point of Second line");
+	        int x1 = scanner.nextInt();
+	        System.out.println("Enter y1 point of Second line");
+	        int x2 = scanner.nextInt();
+	        System.out.println("Enter x2 point of Second line");
+	        
+	System.out.println("Enter x2 point of Second line");
+	        int y1 = scanner.nextInt();
+	        System.out.println("Enter y2 point of Second line");
+	        int y2 = scanner.nextInt();
+	        double length2 = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+	        return length2;
+	 }
+}
